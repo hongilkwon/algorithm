aa729b55b54db027898eec1530836db531421096
홍일권 <hik14@snplab.io>
2023-03-27 23:01:15 +0900

    23.03.27

--- src/bac/week5/R_no_17825.kt
+++ src/bac/week5/R_no_17825.kt
@@ -1,12 +1,26 @@
 package bac.week5
 
+import bac.week1.list
+
 
 /*
 
     주사위 윷놀이
 
+    그래프+구현문제
+
+    1. 기본적인 직사각형이 아닌 독특한 형태의 map을 구현
+    - 생긴 형태가 2차원 배열로 구현할 수 없음.
+    - 인접리스트 형태로 구현해야된다.
+    2개가 인접한 노드일 경우를 주의한다.
+    총 33개의 노드.
+
+    2. 10번의 주사위 값을 4개의 말이 어떻게 나눠서 움직일지  -> 완전탐색
+    3. 최대값
+
  */
 
+val adj = Array(33) { mutableListOf<Int>() }
 
 fun main() = with(System.`in`.bufferedReader()) {
 
