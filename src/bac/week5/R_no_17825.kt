653c75740acb1f78324e10136d441de3d0ca7076
hongilkwon <hong-ilgwon@hong-ilgwon-ui-MacBookAir.local>
2023-03-31 19:57:46 +0900

    230331

--- src/bac/week5/R_no_17825.kt
+++ src/bac/week5/R_no_17825.kt
@@ -20,9 +20,44 @@ import bac.week1.list
 
  */
 
+var dice = emptyArray<Int>()
+
 val adj = Array(33) { mutableListOf<Int>() }
 
+val score = arrayOf(
+    0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
+    22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
+    13, 16, 19, 25, 22, 24, 28, 27, 26, 30,
+    35, 0
+)
+
+fun createAdjArr() {
+    // 시작점 0부터 도착전 노드 까지.
+    for (i in 0..19) {
+        adj[i].add(i + 1)
+    }
+    adj[5].add(21)
+    adj[21].add(22)
+    adj[22].add(23)
+    adj[23].add(24)
+
+    adj[10].add(25)
+    adj[25].add(26)
+    adj[26].add(24)
+
+    adj[15].add(27)
+    adj[27].add(28)
+    adj[28].add(29)
+    adj[29].add(24)
+
+    adj[24].add(30)
+    adj[30].add(31)
+    adj[31].add(20)
+    adj[20].add(32)
+}
+
 fun main() = with(System.`in`.bufferedReader()) {
+    dice = readLine().split(" ").map { it.toInt() }.toTypedArray()
 
 }
 
