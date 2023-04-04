61c138685c64d4369a6161d3659b68ee032bfb3a
hongilkwon <hong-ilgwon@hong-ilgwon-ui-MacBookAir.local>
2023-04-04 11:05:05 +0900

    20230404

--- src/bac/week5/W_no_1912.kt
+++ src/bac/week5/W_no_1912.kt
@@ -1,18 +1,18 @@
 package bac.week5
 
-import kotlin.math.max
-import kotlin.math.min
-
 /*
   연속합
 
-  "연속된 "몇 개의 수를 선택해서 구할 수 있는 합 중 가장 큰 합을 구하려고 한다. 단, 수는 "한 개 이상 선택"
+  "연속된" 몇 개의 수를 선택해서 구할 수 있는 합 중 가장 큰 합을 구하려고 한다. 단, 수는 "한 개 이상 선택"
 
   n(1 ≤ n ≤ 100,000)
   10만 * 10만 반복문 돌면안됨.
 
- */
+  연속된합이라 생각하여 누적합을 이용하려고 하였으나, 작은 음수 나온 후 큰 양수가 나온다면 문제에 맞지가 않는다.
 
+   */
+
+import kotlin.math.max
 
 fun main() = with(System.`in`.bufferedReader()) {
 
@@ -20,39 +20,14 @@ fun main() = with(System.`in`.bufferedReader()) {
     val arr = readLine().split(" ").map { it.toInt() }.toTypedArray()
 
 
-    val sumArr = Array(arr.size) { 0 }
-    sumArr[0] = arr[0]
-
-    for (i in 1 until arr.size) {
-        sumArr[i] += sumArr[i - 1] + arr[i]
-    }
-
-    println(sumArr.joinToString(" "))
-
     var answer = Int.MIN_VALUE
-
-    var minIdx = 0
-    var minValue = Int.MAX_VALUE
-
-    var maxIdx = 0
-    var maxValue = Int.MIN_VALUE
-
-    for (i in 0 until sumArr.size) {
-        if (sumArr[i] >= maxValue) {
-            maxValue = sumArr[i]
-            maxIdx = i
-        }
-        if (sumArr[i] <= minValue) {
-            minValue = sumArr[i]
-            minIdx = i
-        }
-        if (maxIdx >= minIdx){
-            println("maxIdx:$maxIdx  minIdx:$minIdx")
-            answer = sumArr[maxIdx] - sumArr[minIdx]
-        }
+    var sum = 0
+    for (i in 0 until arr.size) {
+        sum += arr[i]
+        answer = max(answer, sum)
+        if (sum < 0) sum = 0
     }
-
-    println("answer:$answer")
+    println(answer)
     Unit
 }
 
---
+++ src/bac/week5/X_no_15683.kt
@@ -0,0 +1,12 @@
+package bac.week5
+
+/*
+  감시
+   */
+
+
+fun main() = with(System.`in`.bufferedReader()) {
+
+    Unit
+}
+
