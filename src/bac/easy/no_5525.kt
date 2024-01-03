7b960210eabab1899198992a7cd3521eebb249a8
홍일권 <hik10417@gmail.com>
2024-01-03 17:46:56 +0900

    240103

--- src/bac/easy/no_5525.kt
+++ src/bac/easy/no_5525.kt
@@ -3,10 +3,49 @@ package bac.easy
 /*
     IOIOI
 
+    * 문자열 매칭에 좋은 문제
+
+    1 ≤ N ≤ 1_000_000
+    2N+1 ≤ M ≤ 1_000_000
+
+    S에 P(n)이 몇 군데 포함되어 있는지 출력한다.
+
+    1) s 문자열을 모두 순회하면서, P(n)과 비교를 한다면?
+    예) M = 1_000_000 이고, N = 1000 이라면  IOIOIOIO...IOI의 길이는 (1000*2)+1
+    1_000_000 * 2000 => 20억 시간초과
+
+    문자열의 특징을 찾는 것이 상당히 중요한 것
+    IOI가 반복
+
  */
 
 
-fun main() = with(System.`in`.bufferedReader()) {
-
-
-}
+//var n = 0
+//var m = 0
+//var s = ""
+//
+//fun main() = with(System.`in`.bufferedReader()) {
+//
+//    n = readLine().toInt()
+//    m = readLine().toInt()
+//    s = readLine()
+//
+//    var i = 0
+//    var cnt = 0
+//    var ioiCnt = 0
+//
+//    while (i < m - 2) {
+//        if (s[i] == 'I' && s[i + 1] == 'O' && s[i + 2] == 'I') {
+//            ioiCnt += 1
+//            if (ioiCnt == n) {
+//                cnt += 1
+//                ioiCnt -= 1
+//            }
+//            i += 2
+//        } else {
+//            ioiCnt = 0
+//            i += 1
+//        }
+//    }
+//    println(cnt)
+//}
--- src/bac/etc/no_1005.kt
+++ src/bac/etc/no_1005.kt
@@ -2,5 +2,11 @@ package bac.etc
 
 /*
     ACM Craft
+
  */
 
+
+fun main()= with(System.`in`.bufferedReader()){
+
+    return@with Unit
+}
