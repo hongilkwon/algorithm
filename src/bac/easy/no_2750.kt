525951eb9d0129f7f41474b815ac572a688d8355
홍일권 <hik10417@gmail.com>
2024-03-14 07:46:08 +0900

    240314

--- src/bac/easy/no_2750.kt
+++ src/bac/easy/no_2750.kt
@@ -1,7 +1,26 @@
 package bac.easy
 
+/*
+   수 정렬하기
+ */
 
-fun main()= with(System.`in`.bufferedReader()){
+var n = 0
 
-   return@with Unit
+fun main() = with(System.`in`.bufferedReader()) {
+
+   n = readLine().toInt()
+
+   val arr = IntArray(n){
+      readLine().toInt()
+   }
+
+   arr.sort()
+
+   val sb = StringBuilder()
+   for(i in 0 until arr.size){
+      sb.append("${arr[i]}\n")
+   }
+
+   println(sb.toString())
+    return@with Unit
 }
